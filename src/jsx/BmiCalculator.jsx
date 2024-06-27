e274fee7471b8ab77c65f6925b635b80495afb70
Jaroslaw2000 <szymanski_ja@o2.pl>
2024-06-27 14:09:24 +0200

    bmi calculator add new function

--- src/jsx/BmiCalculator.jsx
+++ src/jsx/BmiCalculator.jsx
@@ -4,6 +4,7 @@ const BmiCalculator = () => {
     const [weight, setWeight] = useState(0);
     const [height, setHeight] = useState(0);
     const [bmi, setBmi] = useState(0);
+    const [interpretation, setInterpretation] = useState("");
 
 
         const calculateBmi = () => {
@@ -15,9 +16,29 @@ const BmiCalculator = () => {
             return 0;
         }
 
+    const calculateBmiInterpretation = (bmi) => {
+        if (bmi === 0) {
+            return "Podaj poprawne dane";
+        }else if (bmi < 18.5) {
+            return "Niedowaga";
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            return "Waga prawidłowa";
+        } else if (bmi >= 25 && bmi <= 29.9) {
+            return "Nadwaga";
+        } else if (bmi >= 30 && bmi <= 34.9) {
+            return "Otyłość I stopnia";
+        } else if (bmi >= 35 && bmi <= 39.9) {
+            return "Otyłość II stopnia";
+        } else if (bmi >= 40) {
+            return "Otyłość III stopnia";
+        }
+
+    };
 
     useEffect(() => {
+        const newBmi = calculateBmi();
         setBmi(calculateBmi());
+        setInterpretation(calculateBmiInterpretation(newBmi))
     },[height,weight]);
 
 
@@ -28,6 +49,7 @@ const BmiCalculator = () => {
     const handleWeightChange = (e) => {
         setWeight(parseFloat(e.target.value))
     }
+
 
     return (
         <div className="bmi-calculator">
@@ -43,6 +65,12 @@ const BmiCalculator = () => {
             <div>
                 <label>BMI: </label>
                 <span>{bmi}</span>
+            </div>
+            <div>
+                <label>Interpretacja: </label>
+                <br />
+                <span>{interpretation} </span>
+
             </div>
         </div>
     );
